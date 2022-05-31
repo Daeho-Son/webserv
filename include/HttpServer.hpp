0c94481a7bbd96f256812c5d74a234e5414da5c7
JongHyun-GD <58730856+JongHyun-GD@users.noreply.github.com>
2022-05-31 17:36:50 +0900

    Add Simple Echo HelloWorld HttpServer

--- include/HttpServer.hpp
+++ include/HttpServer.hpp
@@ -1,8 +1,16 @@
 #ifndef HTTP_SERVER_HPP
 # define HTTP_SERVER_HPP
 
+# include <iostream>
+# include <netinet/in.h> // sockaddr_in
+# include <string>
+# include <sys/event.h> // kqueue()
+# include <sys/socket.h>
+# include <unistd.h> // close()
+# include <unordered_set>
+# include <vector>
+
 # include "Conf.hpp"
-# include "HttpRequest.hpp"
 
 class HttpServer
 {
@@ -10,12 +18,15 @@ public:
 	HttpServer(Conf& conf);
 	int Run(); // 서버를 실행합니다.
 	int Stop(); // 서버를 종료합니다.
-protected:
 	virtual ~HttpServer();
 private:
 	HttpServer();
 	HttpServer(const HttpServer& other);
 	HttpServer& operator=(const HttpServer& other);
+
+	int addEvent(std::vector<struct kevent>& changeList, uintptr_t ident, int16_t filter, uint16_t flags, uint32_t fflags, intptr_t data, void* udata);
+private:
+	Conf mServerConf;
 };
 
 #endif
