28445a2fa1b1c362e3b0b5a7bf7bb44c745edc9a
JongHyun-GD <58730856+JongHyun-GD@users.noreply.github.com>
2022-06-03 14:44:27 +0900

    Modify main to test response

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include "HttpServer.hpp"
+#include <string>
+#include <iostream>
 
 int main(void)
 {
 	Conf conf(8080, "webserv", 1024);
 	HttpServer server(conf);
 
+	HttpResponse response(200, "Hello, world");
+	std::cout << response.GetHttpMessage() << std::endl;
 	server.Run();
 	server.Stop();
 	return 0;
