de0b98b771464663f9c4619aff80124a94f55bbe
Sondho <thseogh135@gmail.com>
2022-06-14 19:26:12 +0900

    Remove comments

--- test/test.py
+++ test/test.py
@@ -17,16 +17,13 @@ class WebServGetTest(unittest.TestCase) :
 		res = requests.get(self.URL)
 		self.assertEqual(res.status_code, 200)
 
-	# 1회의 GET request 실행
 	def test_get_basic(self):
 		self.get_test()
 
-	# test_size회의 Get request를 순서대로 수행
 	def test_get_massive_sequence(self):
 		for i in range(self.TEST_SIZE):
 			self.get_test()
 
-	# test_size회의 Get request를 thread를 이용해 동시에 수행
 	def test_get_massive_together(self):
 		for i in range(self.TEST_SIZE):
 			t = threading.Thread(target=self.get_test())
