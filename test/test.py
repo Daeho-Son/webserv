f09ccc9f8044c0df641f23828d03fe5460676756
Jonghyun Park <jongpark@c10r8s6.42seoul.kr>
2022-07-17 19:16:48 +0900

    Implement tester scaffold

--- test/test.py
+++ test/test.py
@@ -7,11 +7,80 @@ import sys
 
 class WebServGetTest(unittest.TestCase) :
 	URL = 'http://localhost:8080'
+	SUB_URL = 'http://localhost:8081'
 	TEST_SIZE = 100
 
 	def setUp(self):
 		if (len(sys.argv) == 2):
 			self.TEST_SIZE = int(sys.argv[1])
+
+	def test_200_ok_get(self):
+		res = requests.get(self.URL)
+		self.assertEqual(res.status_code, 200)
+
+	def test_200_ok_head(self):
+		res = requests.head(self.URL)
+		self.assertEqual(res.status_code, 200)
+
+	def test_201_created_put(self):
+		res = requests.put(self.URL + "/put_test/test_temp_output", data="201_created_put")
+		self.assertEqual(res.status_code, 201)
+
+	def test_204_post_exist(self):
+		res = requests.post(self.URL + "/put_test/test_temp_output", data="204_post_exist")
+		self.assertEqual(res.status_code, 204)
+
+	def test_204_post_new(self):
+		res = requests.post(self.URL + "/put_test/not_exist", data="204_post_new")
+		self.assertEqual(res.status_code, 204)
+	
+	def test_204_put_exist(self):
+		res = requests.post(self.URL + "/put_test/test_temp_output", data="204_put_exist")
+		self.assertEqual(res.status_code, 204)	
+
+	def test_400_no_method_exist(self):
+		res = requests.request("NO_METHOD", self.URL + "/put_test/test_temp_output")
+		self.assertEqual(res.status_code, 400)
+
+	def test_400_no_method_new(self):
+		res = requests.request("NO_METHOD", self.URL + "/put_test/not_exist")
+		self.assertEqual(res.status_code, 400)
+
+	def test_404_not_found_get(self):
+		res = requests.get(self.URL + "/i_dont_have_this_page")
+		self.assertEqual(res.status_code, 404)
+
+	def test_404_not_found_head(self):
+		res = requests.head(self.URL + "/i_dont_have_this_page")
+		self.assertEqual(res.status_code, 404)
+
+	def test_405_method_not_allowed_get(self):
+		res = requests.get(self.URL + "/post_body/index.html")
+		self.assertEqual(res.status_code, 405)
+		
+	def test_405_method_not_allowed_post(self):
+		res = requests.post(self.URL)
+		self.assertEqual(res.status_code, 405)
+
+	def test_411_length_required_post(self):
+		res = requests.request("POST", self.URL + "/post_body/index.html")
+		self.assertEqual(res.status_code, 411)
+
+	def test_411_length_required_post(self):
+		res = requests.request("PUT", self.URL + "/put_test/new.html")
+		self.assertEqual(res.status_code, 411)
+
+	def test_413_payload_too_large_post(self):
+		res = requests.post(self.URL + "/post_body/index.html", data=("a"*10000))
+		self.assertEqual(res.status_code, 413)
+
+	def test_virtual_hosting_1(self):
+		res = requests.get(self.URL, headers={"Host", "webserv"})
+		self.assertEqual(res.status_code, 200)
+
+	def test_virtual_hosting_2(self):
+		res = requests.get(URL, headers={"Host", "webserv1"})
+		self.assertEqual(res.status_code, 200)
 
 	def get_test(self):
 		res = requests.get(self.URL)
@@ -20,65 +89,14 @@ class WebServGetTest(unittest.TestCase) :
 	def test_get_basic(self):
 		self.get_test()
 
-	def test_get_massive_sequence(self):
-		for i in range(self.TEST_SIZE):
-			self.get_test()
+	# def test_get_massive_sequence(self):
+	# 	for i in range(self.TEST_SIZE):
+	# 		self.get_test()
 
-	def test_get_massive_together(self):
-		for i in range(self.TEST_SIZE):
-			t = threading.Thread(target=self.get_test())
-			t.start()
-
-	def test_204_no_content(self):
-		res = requests.get(self.URL)
-		if res.content == "":
-			self.assertEqual(res.status_code, 204)
-
-	# 400 Bad Request: 이 응답은 잘못된 문법으로 인하여 서버가 요청하여 이해할 수 없음을 의미합니다.
-	def test_400_bad_request(self):
-		headers={'Connection':''}
-		res = requests.get(self.URL, headers=headers)
-		print(res.request.headers)
-		self.assertEqual(res.status_code, 400)
-
-	# def test_403_forbidden(self):
-	# 	res = requests.get(self.URL)
-	#	self.assertEqual(res.status_code, 403)
-
-	def test_404_not_found(self):
-		HTML_URL = f'{self.URL}/kk'
-		res = requests.get(HTML_URL)
-		self.assertEqual(res.status_code, 404)
-
-	# localhost:8080/ 은 GET만 가능
-	def test_405_method_not_allowed(self):
-		res = requests.put(self.URL)
-		self.assertEqual(res.status_code, 405)
-
-	def test_408_request_timeout(self):
-		res = requests.get(self.URL, timeout=100)
-		self.assertEqual(res.status_code, 408)
-
-	# def test_411_length_required(self):
-	# 	res = requests.get(self.URL)
-	# 	self.assertEqual(res.status_code, 411)
-
-	# def test_429_too_many_requests(self):
-	# 	res = requests.get(self.URL)
-	# 	self.assertEqual(res.status_code, 429)
-
-	# def test_500_internal_server_error(self):
-	# 	res = requests.get(self.URL)
-	# 	self.assertEqual(res.status_code, 500)
-
-	# def test_501_not_implemented(self):
-	# 	res = requests.get(self.URL)
-	# 	self.assertEqual(res.status_code, 501)
-
-	# def test_test(self):
-	# 	headers = {'Connection':'test'}
-	# 	res = requests.request('GET', url=self.URL, headers=headers)
-	# 	print(res.request)
+	# def test_get_massive_together(self):
+	# 	for i in range(self.TEST_SIZE):
+	# 		t = threading.Thread(target=self.get_test())
+	# 		t.start()
 	
 if __name__ == '__main__':
 	if len(sys.argv) <= 2:
